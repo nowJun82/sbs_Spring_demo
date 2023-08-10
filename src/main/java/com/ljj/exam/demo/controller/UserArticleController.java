4e6e1dc56e8f2719984bf005b39a1f968fe415dd
nowJun82 <oizill5481@icloud.com>
2023-08-10 13:44:49 +0900

    article 기능 부분 추가 및 실행 시 테스트데이터 10개 생성

---
+++ src/main/java/com/ljj/exam/demo/controller/UserArticleController.java
@@ -0,0 +1,56 @@
+package com.ljj.exam.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ljj.exam.demo.vo.Article;
+
+@Controller
+public class UserArticleController {
+	private int articleLastId;
+	private List<Article> articles;
+
+	public UserArticleController() {
+		articleLastId = 0;
+		articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+	
+	@RequestMapping("user/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		int id = articleLastId + 1;
+
+		Article article = new Article(id, title, body);
+		articles.add(article);
+		
+		articleLastId = id;
+		
+		return article;
+	}
+	
+	@RequestMapping("user/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		return articles;
+	}
+	
+	public void makeTestData() {
+		
+		for (int i = 1; i <= 10; i++) {
+			int id = articleLastId + 1;
+			
+			String title = "제목"+i;
+			String body = "내용"+i;
+			Article article = new Article(id, title, body);
+			articles.add(article);
+			
+			articleLastId = id;
+		}
+	}
+}
