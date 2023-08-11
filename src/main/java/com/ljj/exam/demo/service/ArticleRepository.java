298083f279b6aa15edc40562a61a77cefaa3ec57
nowJun82 <oizill5481@icloud.com>
2023-08-11 13:22:55 +0900

    ArticleRepository와 DB간 쿼리문 annotation / last_insert_id 사용

--- src/main/java/com/ljj/exam/demo/service/ArticleRepository.java
+++ src/main/java/com/ljj/exam/demo/service/ArticleRepository.java
@@ -2,23 +2,33 @@ package com.ljj.exam.demo.service;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.ljj.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-
-	public Article writeArticle(String title, String body);
 	
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public Article writeArticle(@Param("title") String title, @Param("body") String body);
+	
+	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
-
-	// SELECT * FROM article WHERE id = ?
+	
 	@Select("SELECT * FROM article WHERE id = #{id}")
-	public Article getArticle(int id);
+	public Article getArticle(@Param("id") int id);
+	
+	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
+	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
-	public void modifyArticle(int id, String title, String body);
-
-	public void deleteArticle(int id);
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
+	
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
 }
