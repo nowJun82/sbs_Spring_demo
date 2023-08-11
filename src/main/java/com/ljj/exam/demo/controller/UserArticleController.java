5a433dcafa464fe9446a37c48693bccb85f8dab1
nowJun82 <oizill5481@icloud.com>
2023-08-11 10:48:08 +0900

    게시물 목록, 수정, 삭제 등 구현과 동시에 ArticleService로 이동

--- src/main/java/com/ljj/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/ljj/exam/demo/controller/UserArticleController.java
@@ -3,54 +3,71 @@ package com.ljj.exam.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ljj.exam.demo.service.ArticleService;
 import com.ljj.exam.demo.vo.Article;
 
 @Controller
 public class UserArticleController {
-	private int articleLastId;
-	private List<Article> articles;
+	@Autowired
+	private ArticleService articleService;
 
-	public UserArticleController() {
-		articleLastId = 0;
-		articles = new ArrayList<>();
-		
-		makeTestData();
-	}
-	
-	@RequestMapping("user/article/doAdd")
+	// doAdd
+	@RequestMapping("/user/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		int id = articleLastId + 1;
-
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		
-		articleLastId = id;
-		
+		Article article = articleService.writeArticle(title, body);
 		return article;
 	}
 	
-	@RequestMapping("user/article/getArticles")
+	// getArticles
+	@RequestMapping("/user/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		return articles;
+		return articleService.getArticles();
 	}
-	
-	public void makeTestData() {
-		
-		for (int i = 1; i <= 10; i++) {
-			int id = articleLastId + 1;
-			
-			String title = "제목"+i;
-			String body = "내용"+i;
-			Article article = new Article(id, title, body);
-			articles.add(article);
-			
-			articleLastId = id;
+
+	// getArticle
+	@RequestMapping("/user/article/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id) {
+		Article article = articleService.getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
 		}
+		return article;
+	}
+
+	// doModify
+	@RequestMapping("/user/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = articleService.getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		articleService.modifyArticle(id, title, body);
+
+		return id + "번 게시물이 수정되었습니다.";
+	}
+
+	// doDelete
+	@RequestMapping("/user/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = articleService.getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		articleService.deleteArticle(id);
+
+		return id + "번 게시물이 삭제되었습니다.";
 	}
 }
--- src/main/java/com/ljj/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/ljj/exam/demo/controller/UserHomeController.java
@@ -6,29 +6,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserHomeController {
-	private int result;
-	
-	public UserHomeController() {
-		result = 0;
-	}
-	
 	// 사용자가 아래와 같은 경로로 접근하면 아래 showMain() 함수 실행
 	@RequestMapping("user/home/main")
 	@ResponseBody
 	public String showMain() {
-		return "안녕하세요";
-	}
-	
-	@RequestMapping("user/home/getCount")
-	@ResponseBody
-	public int getCount() {
-		return result++;
-	}
-	
-	@RequestMapping("user/home/doSetCount")
-	@ResponseBody
-	public String doSetCount(int result) {	// 인자를 넘겨줘야 함
-		this.result = result;
-		return "값이 " + result + "으로 초기화되었습니다.";
+		return "메인입니다.";
 	}
 }
